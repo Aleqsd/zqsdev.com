f7c42c0ca85192e6825ebc0f36ce40ae3d4e7506
Aleqsd <aleqsd@gmail.com>
2025-11-02 11:16:59 +0100

    fix: Update project links and correct technology names in JSON; enhance TypeScript definitions for WASM bindings

--- static/pkg/zqs_terminal.js
+++ static/pkg/zqs_terminal.js
@@ -222,12 +222,12 @@ function wasm_bindgen__convert__closures_____invoke__h0ca27dde890ea589(arg0, arg
     wasm.wasm_bindgen__convert__closures_____invoke__h0ca27dde890ea589(arg0, arg1, arg2);
 }
 
-function wasm_bindgen__convert__closures_____invoke__h7ec6555be686a90f(arg0, arg1) {
-    wasm.wasm_bindgen__convert__closures_____invoke__h7ec6555be686a90f(arg0, arg1);
-}
-
 function wasm_bindgen__convert__closures_____invoke__h13fe8b1350e448f2(arg0, arg1, arg2) {
     wasm.wasm_bindgen__convert__closures_____invoke__h13fe8b1350e448f2(arg0, arg1, arg2);
+}
+
+function wasm_bindgen__convert__closures_____invoke__h7ec6555be686a90f(arg0, arg1) {
+    wasm.wasm_bindgen__convert__closures_____invoke__h7ec6555be686a90f(arg0, arg1);
 }
 
 const __wbindgen_enum_RequestMode = ["same-origin", "no-cors", "cors", "navigate"];
